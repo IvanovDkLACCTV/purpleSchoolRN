ca23bfcf4059b06dbf4129a4428e340094b5dde7
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-11-01 09:03:22 +0300

    started editting form layout

--- native-app/App.tsx
+++ native-app/App.tsx
@@ -1,14 +1,53 @@
+//outer exports
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Dimensions,
+  TextInput,
+} from "react-native";
+//inner exports
+import { Colors } from "./constants/Colors";
+import { width } from "./constants/Sizes";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.textStyle}>Welcome back, dude</Text>
+        <View style={styles.form}>
+          <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
+          <Button title="Enter" />
+        </View>
+        <Text style={styles.textStyle}>Reset your password</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 70,
+    justifyContent: "center",
+    flex: 1,
+    padding: 55,
   },
-  wrapper: {},
-  text: {},
+  content: {
+    alignItems: "center",
+    gap: 50,
+  },
+  textStyle: {
+    color: Colors.light.tint,
+    fontSize: 24,
+  },
+  form: {
+    alignSelf: "stretch",
+    gap: 16,
+  },
+  input: {
+    backgroundColor: Colors.light.background,
+    color: Colors.light.text,
+  },
 });
--- native-app/constants/Colors.ts
+++ native-app/constants/Colors.ts
@@ -1,21 +1,26 @@
-const tintColorLight = "#0a7ea4";
+const tintColorLight = "#000";
 const tintColorDark = "#fff";
+const accentColorLight = "tomato";
+const accentColorDark = "red";
+const purpleBackground = "#2E2D3D";
 
 export const Colors = {
   light: {
     text: "#11181C",
-    background: "#fff",
+    background: "lightgray",
     tint: tintColorLight,
     icon: "#687076",
     tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
+    accent: accentColorLight,
   },
   dark: {
     text: "#ECEDEE",
-    background: "#151718",
+    background: purpleBackground,
     tint: tintColorDark,
     icon: "#9BA1A6",
     tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
+    accent: accentColorDark,
   },
 };

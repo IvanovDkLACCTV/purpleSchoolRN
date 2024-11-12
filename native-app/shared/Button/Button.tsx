1bb6f8099cc11d67be4dafac1561118238e4e428
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-11-12 15:27:35 +0300

    useNativeDriver changed to false, because it doesn't work well with simulator

--- native-app/shared/Button/Button.tsx
+++ native-app/shared/Button/Button.tsx
@@ -1,65 +1,79 @@
-import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps, Pressable, View, Animated, GestureResponderEvent } from 'react-native'
-import { Theme } from '../../constants/Colors'
-import { Radius, FontSize, Colors } from '../tokens'
-import { useEffect } from 'react'
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Pressable,
+  View,
+  Animated,
+  GestureResponderEvent,
+} from "react-native";
+import { Theme } from "../../constants/Colors";
+import { Radius, FontSize, Colors } from "../tokens";
+import { useEffect } from "react";
 
 interface ButtonProps extends TouchableOpacityProps {
-  title: string
-  isDarkMode?: boolean
+  title: string;
+  isDarkMode?: boolean;
 }
 
-export const Button = ({ title, isDarkMode = false, style, ...props }: ButtonProps) => {
-  const theme = isDarkMode ? Theme.dark : Theme.light
-  const animatedValue = new Animated.Value(100)
+export const Button = ({
+  title,
+  isDarkMode = false,
+  style,
+  ...props
+}: ButtonProps) => {
+  const theme = isDarkMode ? Theme.dark : Theme.light;
+  const animatedValue = new Animated.Value(100);
   const color = animatedValue.interpolate({
     inputRange: [0, 100],
     outputRange: [theme.hover, theme.preHover],
-  })
+  });
 
   const fadeIn = (e: GestureResponderEvent) => {
     Animated.timing(animatedValue, {
       toValue: 0,
       duration: 200,
-      useNativeDriver: true,
-    }).start()
-    props.onPressIn && props.onPressIn(e)
-  }
+      useNativeDriver: false,
+    }).start();
+    props.onPressIn && props.onPressIn(e);
+  };
 
   const fadeOut = (e: GestureResponderEvent) => {
     Animated.timing(animatedValue, {
       toValue: 100,
       duration: 200,
-      useNativeDriver: true,
-    }).start()
-    props.onPressOut && props.onPressOut(e)
-  }
+      useNativeDriver: false,
+    }).start();
+    props.onPressOut && props.onPressOut(e);
+  };
 
   return (
-    <Pressable
-      {...props}
-      onPressIn={fadeIn}
-      onPressOut={fadeOut}>
+    <Pressable {...props} onPressIn={fadeIn} onPressOut={fadeOut}>
       <Animated.View
         style={[
           styles.button,
           {
             backgroundColor: color,
           },
-        ]}>
-        <Text style={[styles.buttonText, { color: Theme.dark.text }]}>{title}</Text>
+        ]}
+      >
+        <Text style={[styles.buttonText, { color: Theme.dark.text }]}>
+          {title}
+        </Text>
       </Animated.View>
     </Pressable>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   button: {
     padding: 12,
     borderRadius: Radius.r10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   buttonText: {
-    fontFamily: 'Poppins_400Regular',
+    fontFamily: "Poppins_400Regular",
     fontSize: FontSize.f16,
   },
-})
+});

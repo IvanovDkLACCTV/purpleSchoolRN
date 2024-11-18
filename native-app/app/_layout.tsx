8b1f67392180466d94fea17142c672a1c3865f45
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-11-18 13:59:22 +0300

    tried to make dark theme for statusBar

--- native-app/app/_layout.tsx
+++ native-app/app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Slot, Stack, Tabs } from "expo-router";
+import { useState } from "react";
+import { Theme } from "../constants/Colors";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const theme = isDarkMode ? Theme.dark : Theme.light;
+  return( 
+    <Stack screenOptions={{ 
+      headerShown: false, 
+      statusBarColor: theme.background,
+      statusBarStyle: isDarkMode ? 'light' : 'dark'
+     }}> 
+    <Stack.Screen name="index" options={{ headerShown: false, statusBarColor: theme.background, statusBarStyle: isDarkMode ? 'light' : 'dark' }} />
+    <Stack.Screen name="restore" options={{ headerShown: false, presentation: 'modal', statusBarColor: theme.background, statusBarStyle: isDarkMode ? 'light' : 'dark'  }} />
+  </Stack>)
 }

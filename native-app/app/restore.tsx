b83ac4339b4e7d612e32f72b4088786367c52eb7
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-11-18 13:34:53 +0300

    set statusBarColor for restore component

--- native-app/app/restore.tsx
+++ native-app/app/restore.tsx
@@ -1,10 +1,15 @@
 import { Stack, Link } from "expo-router";
 import { View, Text } from "react-native";
+import { useState } from "react";
+
+import { Theme } from "../constants/Colors";
 
 export default function Restore() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const theme = isDarkMode ? Theme.dark : Theme.light;
     return (
     <View>
-      <Stack.Screen options={{ title: "Restore" }} />
+      <Stack.Screen options={{ title: "Restore password", statusBarColor: theme.background }} />
       <Link href={"/"}>
         <Text>Restore</Text>
       </Link>

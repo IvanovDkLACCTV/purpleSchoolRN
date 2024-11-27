49a8f9d5357e21b8029b354c15f2a658f430cf10
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-11-27 13:48:28 +0300

    tested React Native Async Storage

--- native-app/app/(app)/index.tsx
+++ native-app/app/(app)/index.tsx
@@ -1,9 +1,21 @@
 import { useAtom } from "jotai";
 import { Text, View } from "react-native";
 import { profileAtom } from "../../entities/user/model/user.state";
+import { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function MyCourses() {
     const [profile] = useAtom(profileAtom)
+
+    useEffect(() => {
+        AsyncStorage.setItem('demo', 'test').then(async () => {
+            console.log(await AsyncStorage.getAllKeys())
+            console.log(await AsyncStorage.getItem('demo'))
+            console.log(await AsyncStorage.removeItem('demo'))
+            console.log(await AsyncStorage.getAllKeys())
+        })
+    }, [])
+
     return <View>
         <Text>{profile.profile?.name}</Text>
     </View>

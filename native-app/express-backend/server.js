9f31d6ff9a349610a2f1a0d8fb92f727fe8a335b
Dik131 <d.i.k.93@mail.ru>
2024-12-02 21:41:39 +0300

    Finally wrote right localhost adress and now I can get data from my node server

--- native-app/express-backend/server.js
+++ native-app/express-backend/server.js
@@ -1,25 +1,27 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const jwt = require("jsonwebtoken")
-const users = require("./users")
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const users = require('./users');
+const cors = require('cors');
 
-const app = express()
-const port = 3000
-const secretKey = "a-very-secret-key-123456"
+const app = express();
+const port = 3000;
+const secretKey = 'a-very-secret-key-123456';
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
-app.post("/api-v2/auth/login", (req, res) => {
-  const { email, password } = req.body
+app.post('/api-v2/auth/login', (req, res) => {
+  const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({ error: "Email and password are required" })
+    return res.status(400).json({ error: 'Email and password are required' });
   }
 
-  const user = users.find((u) => u.email === email && u.password === password)
+  const user = users.find((u) => u.email === email && u.password === password);
 
   if (!user) {
-    return res.status(401).json({ error: "Invalid credentials" })
+    return res.status(401).json({ error: 'Invalid credentials' });
   }
 
   const token = jwt.sign(
@@ -30,12 +32,12 @@ app.post("/api-v2/auth/login", (req, res) => {
       email: user.email,
     },
     secretKey,
-    { expiresIn: "1h" } // Токен действует 1 час
-  )
+    { expiresIn: '1h' } // Токен действует 1 час
+  );
 
-  res.status(200).json({ access_token: token, user })
-})
+  res.status(200).json({ access_token: token, user });
+});
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
--- native-app/express-backend/users.js
+++ native-app/express-backend/users.js
@@ -1,12 +1,12 @@
 const users = [
   {
-    id: "1",
-    name: "John",
-    surname: "Doe",
-    email: "john.doe@example.com",
-    photo: "",
-    password: "password123", // Для реального проекта хешируйте пароли
+    id: '1',
+    name: 'Bradly',
+    surname: 'Doe',
+    email: 'john.doe@example.com',
+    photo: '',
+    password: 'password123', // Для реального проекта хешируйте пароли
   },
-]
+];
 
-module.exports = users
+module.exports = users;

6ff1efeb7161d5d372124af5550681618664e1f0
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-12-10 13:39:57 +0300

    customized index for theme switch

--- native-app/app/(app)/index.tsx
+++ native-app/app/(app)/index.tsx
@@ -1,14 +1,39 @@
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet, Switch } from "react-native"
 import { Button } from "../../shared/Button/Button"
 import { useSetAtom } from "jotai"
 import { logoutAtom } from "../../entities/auth/model/auth.state"
+import { useTheme } from "../../shared/ThemeContext"
+import { Theme } from "../../constants/Colors"
 
 export default function MyCourses() {
+  const { isDarkMode, setIsDarkMode } = useTheme()
+  const theme = isDarkMode ? Theme.dark : Theme.light
+
   const logout = useSetAtom(logoutAtom)
   return (
-    <View>
-      <Text></Text>
-      <Button title="Logout" onPress={logout} />
+    <View style={styles.container}>
+      <Text style={{ color: theme.text }}>My Courses</Text>
+      <Button title="Logout" onPress={logout} isDarkMode={isDarkMode} />
+      <View style={styles.bottom}>
+        <Switch
+          value={isDarkMode}
+          onValueChange={setIsDarkMode}
+          thumbColor={theme.tint}
+          trackColor={{ false: theme.lighter, true: theme.lighter }}
+        />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Theme.light.background,
+    justifyContent: "center",
+    flex: 1,
+    padding: 20,
+  },
+  bottom: {
+    marginTop: "auto",
+  },
+})
--- native-app/express-backend/users.js
+++ native-app/express-backend/users.js
@@ -1,12 +1,20 @@
 const users = [
   {
-    id: '1',
-    name: 'Bradly',
-    surname: 'Doe',
-    email: 'john.doe@example.com',
-    photo: '',
-    password: 'password123', // Для реального проекта хешируйте пароли
+    id: "1",
+    name: "Bradly",
+    surname: "Doe",
+    email: "john.doe@example.com",
+    photo: "",
+    password: "password123", // Для реального проекта хешируйте пароли
   },
-];
+  {
+    id: "2",
+    name: "Name",
+    surname: "Surname",
+    email: "user@mail.com",
+    photo: "",
+    password: "123456", // Для реального проекта хешируйте пароли
+  },
+]
 
-module.exports = users;
+module.exports = users

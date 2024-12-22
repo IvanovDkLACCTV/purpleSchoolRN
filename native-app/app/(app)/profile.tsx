d8e8b661309c55e2466edd6e334a1a14804d7a9d
Dik131 <d.i.k.93@mail.ru>
2024-12-22 16:55:22 +0300

    separeted image picker and camera

--- native-app/app/(app)/profile.tsx
+++ native-app/app/(app)/profile.tsx
@@ -1,6 +1,6 @@
 import { Text, View, StyleSheet, Image, Alert } from 'react-native'
 import { useState } from 'react'
-import { launchImageLibraryAsync, PermissionStatus, useCameraPermissions } from 'expo-image-picker'
+import { launchImageLibraryAsync, launchCameraAsync, PermissionStatus, useMediaLibraryPermissions, useCameraPermissions } from 'expo-image-picker'
 
 import ThemeSwitch from '../../shared/ThemeSwitch/ThemeSwitch'
 import { useTheme } from '../../shared/ThemeSwitch/ThemeContext'
@@ -13,27 +13,73 @@ export default function Profile() {
 
   // image picker
   const [image, setImage] = useState<string | null>(null)
+  const [mediaLibraryPermission, requestMediaLibraryPermission] = useMediaLibraryPermissions()
   const [cameraPermission, requestCameraPermission] = useCameraPermissions()
 
-  const varifyCameraPermissions = async () => {
+  const verifyMediaLibraryPermissions = async () => {
+    if (!mediaLibraryPermission) {
+      const permission = await requestMediaLibraryPermission()
+      return permission.granted
+    }
+
+    if (mediaLibraryPermission.status === PermissionStatus.UNDETERMINED) {
+      const permission = await requestMediaLibraryPermission()
+      return permission.granted
+    }
+
+    if (mediaLibraryPermission.status === PermissionStatus.DENIED) {
+      Alert.alert('Permission Required', 'Please allow access to your photo library in settings', [
+        { text: 'OK', onPress: () => requestMediaLibraryPermission() },
+        { text: 'Cancel', style: 'cancel' },
+      ])
+      return false
+    }
+
+    return mediaLibraryPermission.status === PermissionStatus.GRANTED
+  }
+
+  const verifyCameraPermissions = async () => {
+    if (cameraPermission?.status === PermissionStatus.GRANTED) {
+      return true
+    }
+
     if (cameraPermission?.status === PermissionStatus.UNDETERMINED) {
       const res = await requestCameraPermission()
       return res.granted
     }
+
     if (cameraPermission?.status === PermissionStatus.DENIED) {
-      Alert.alert('Allow camera for this app in phone settings')
+      Alert.alert('Permission Required', 'Please allow access to your camera in settings')
       return false
     }
-    return true
+
+    return false
+  }
+
+  const takePhoto = async () => {
+    const isPermissionGranted = await verifyCameraPermissions()
+    if (!isPermissionGranted) {
+      return
+    }
+
+    const result = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    })
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri)
+    }
   }
 
   const pickImage = async () => {
-    const isPermissionGranted = await varifyCameraPermissions()
+    const isPermissionGranted = await verifyMediaLibraryPermissions()
     if (!isPermissionGranted) {
       return
     }
     let result = await launchImageLibraryAsync({
-      mediaTypes: ['images'],
+      mediaTypes: 'images',
       allowsEditing: true,
       aspect: [1, 1],
       quality: 1,
@@ -49,10 +95,12 @@ export default function Profile() {
       justifyContent: 'center',
       flex: 1,
       padding: 20,
+      gap: 20,
     },
     bottom: {
       marginTop: 'auto',
     },
+    image: { width: 100, height: 100, alignSelf: 'center' },
   })
 
   return (
@@ -60,11 +108,17 @@ export default function Profile() {
       {image && (
         <Image
           source={{ uri: image }}
-          style={{ width: 100, height: 100 }}
+          style={styles.image}
         />
       )}
       <Button
-        title="Pick an avatar"
+        title="Take a photo"
+        onPress={takePhoto}
+        isDarkMode={isDarkMode}
+      />
+
+      <Button
+        title="Pick from gallery"
         onPress={pickImage}
         isDarkMode={isDarkMode}
       />

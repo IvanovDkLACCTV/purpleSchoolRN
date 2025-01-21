e699c31b79518b6656b9629aa6f9496d237113e5
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-01-21 14:48:41 +0300

    I broke everything

--- native-app/app/(app)/index.tsx
+++ native-app/app/(app)/index.tsx
@@ -1,6 +1,12 @@
-import { View, StyleSheet, Text } from "react-native"
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native"
 import { useAtomValue, useSetAtom } from "jotai"
-import { logoutAtom } from "../../entities/auth/model/auth.state"
 import { useTheme } from "../../shared/ThemeSwitch/ThemeContext"
 import { Theme } from "../../constants/Colors"
 import ThemeSwitch from "../../shared/ThemeSwitch/ThemeSwitch"
@@ -10,6 +16,8 @@ import {
 } from "../../entities/course/model/course.state"
 import { useEffect } from "react"
 import { CourseCard } from "../../entities/course/ui/CourseCard/CourseCard"
+import { StudentCourseDescription } from "../../entities/course/model/course.model"
+import { Colors } from "../../shared/tokens"
 
 export default function MyCourses() {
   const { isDarkMode } = useTheme()
@@ -26,6 +34,12 @@ export default function MyCourses() {
     bottom: {
       marginTop: "auto",
     },
+    item: {
+      padding: 20,
+    },
+    activity: {
+      marginTop: 30,
+    },
   })
 
   const loadCourse = useSetAtom(loadCourseAtom)
@@ -34,10 +48,40 @@ export default function MyCourses() {
     loadCourse()
   }, [])
 
+  const renderCourse = ({ item }: { item: StudentCourseDescription }) => {
+    return (
+      <View style={styles.item}>
+        <CourseCard {...item} />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      {courses.length > 0 &&
-        courses.map((c) => <CourseCard key={c.id} {...c} />)}
+      {isLoading && (
+        <ActivityIndicator
+          style={styles.activity}
+          size="large"
+          color={Colors.primary}
+        />
+      )}
+      {courses.length > 0 ? (
+        <FlatList
+          refreshControl={
+            <RefreshControl
+              tintColor={Colors.primary}
+              titleColor={Colors.primary}
+              refreshing={isLoading}
+              onRefresh={loadCourse}
+            />
+          }
+          data={courses}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={renderCourse}
+        />
+      ) : (
+        <Text>No courses available</Text>
+      )}
       <View style={styles.bottom}>
         <ThemeSwitch />
       </View>
